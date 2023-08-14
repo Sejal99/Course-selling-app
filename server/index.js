69b9ca397293c01f52fec3a242b9eddc26b83d8d
sejal99 <sejal8974@gmail.com>
2023-08-14 13:42:23 +0530

    new commit

--- server/index.js
+++ server/index.js
@@ -7,9 +7,9 @@ const dotenv=require("dotenv");
 dotenv.config()
 const app = express();
 
-app.use(cors({
-    origin:'https://sejal-course-selling-app.vercel.app/'
-}));
+app.use(cors(
+    'https://sejal-course-selling-app.vercel.app/'
+));
 app.use(express.json());
 
 app.use("/admin", adminRouter)
