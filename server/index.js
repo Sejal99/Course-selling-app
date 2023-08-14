7324e8b06299483f6ea1b8eb659b1dbf0d0fc24a
sejal99 <sejal8974@gmail.com>
2023-08-14 12:40:51 +0530

    first commit

---
+++ server/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const adminRouter = require("./routes/admin");
+const userRouter = require("./routes/user");
+const dotenv=require("dotenv");
+dotenv.config()
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/admin", adminRouter)
+app.use("/user", userRouter)
+
+
+// Connect to MongoDB
+// DONT MISUSE THIS THANKYOU!!
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true, dbName: "courses" })
+ .then(()=>console.log('database connected'))
+ .catch((err)=>console.log(err))
+
+app.listen(3000, () => console.log('Server running on port 3000'));
