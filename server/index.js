e8f04a1f6368cb079c9530c0100b44c4b33d388a
sejal99 <sejal8974@gmail.com>
2023-08-14 13:04:00 +0530

    recent commit

--- server/index.js
+++ server/index.js
@@ -7,7 +7,9 @@ const dotenv=require("dotenv");
 dotenv.config()
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin:'https://sejal-course-selling-app.vercel.app/'
+}));
 app.use(express.json());
 
 app.use("/admin", adminRouter)
@@ -20,4 +22,4 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
  .then(()=>console.log('database connected'))
  .catch((err)=>console.log(err))
 
-app.listen(3000, () => console.log('Server running on port 3000'));
+app.listen(process.env.PORT||3001, () => console.log('Server running on port ' + process.env.PORT||3001 ));
